afa0a819c58d6c440b0aa149326c61e3134882a3
GilangDafa10 <gilangdafa64@gmail.com>
2025-12-30 15:24:35 +0700

    debug awal setelah fork dari repo asli dan clone dari repo saya

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -15,9 +16,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@800;900&display=swap" rel="stylesheet" />
 
     <!-- Vite -->
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @viteReactRefresh @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
+
 <body class="font-sans antialiased">
     <div id="app"></div>
 </body>
+
 </html>
